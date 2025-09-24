8d99f31623ea346fd3589e863b3a928c4d6ec88e
mominramjan07 <mominramjan786786@gmail.com>
2025-09-24 14:27:58 +0530

    Updated code

--- VMC/settings.py
+++ VMC/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'replace-this-with-a-secure-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
