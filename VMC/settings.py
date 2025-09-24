7956f9b55ee069f7331af01f756cbb3523927401
mominramjan07 <mominramjan786786@gmail.com>
2025-09-25 01:27:30 +0530

    Fix project name for Render deployment

--- VMC/settings.py
+++ VMC/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "MominKart",   # आपका app
+    "VMC",   # आपका app
 ]
 
 # ---------------- Middleware ----------------
